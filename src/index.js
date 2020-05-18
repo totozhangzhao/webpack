2cdcfd5221e3546dd294f0ee93425fd8c8417dd0
totozhangzhao <totozhangzhao@163.com>
2020-05-18 23:17:06 +0800

    feat: 加载图片

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import _ from 'lodash';
+import './index.css';
+import Kobe from './img/kobe1.jpg';
 
 function component() {
   let element = document.createElement('div');
   element.innerHTML = _.join(['Hello new ', 'World'], '');
+  element.classList.add('red');
+
+  let img = new Image();
+  img.src = Kobe;
+
+  element.appendChild(img);
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,22 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
   }
 };
