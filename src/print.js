098aafadeb35e59de2a0762d2f6e2d55641f89ee
zhangzhao <zhangz@133.cn>
2020-06-18 15:20:36 +0800

    feat: lazy-load

--- src/print.js
+++ src/print.js
@@ -1,4 +1,5 @@
-export default function printMe() {
-  console.log('I get called from print.js! new');
-  // console.error('I get called from print.js new');
-}
+console.log('The print.js module has loaded! See the network tab is dev tools...');
+
+export default () => {
+  console.log('Button Clicked: Here\'s "some text"!');
+};
